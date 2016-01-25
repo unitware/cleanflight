ad6f4c0285c34bd6b532caf6abf7a7ac805e79ba
Craig J Perry <craigp84@gmail.com>
2016-01-25 03:53:39 +0000

    Add ibus telemetry support

--- src/main/telemetry/telemetry.c
+++ src/main/telemetry/telemetry.c
@@ -43,6 +43,7 @@
 #include "telemetry/smartport.h"
 #include "telemetry/ltm.h"
 #include "telemetry/mavlink.h"
+#include "telemetry/ibus.h"
 
 PG_REGISTER_WITH_RESET_TEMPLATE(telemetryConfig_t, telemetryConfig, PG_TELEMETRY_CONFIG, 0);
 
@@ -65,6 +66,8 @@ void telemetryInit(void)
     initSmartPortTelemetry();
     initLtmTelemetry();
     initMAVLinkTelemetry();
+    initIbusTelemetry();
+
     telemetryCheckState();
 }
 
@@ -89,6 +92,7 @@ void telemetryCheckState(void)
     checkSmartPortTelemetryState();
     checkLtmTelemetryState();
     checkMAVLinkTelemetryState();
+    checkIbusTelemetryState();
 }
 
 void telemetryProcess(uint16_t deadband3d_throttle)
@@ -98,6 +102,7 @@ void telemetryProcess(uint16_t deadband3d_throttle)
     handleSmartPortTelemetry();
     handleLtmTelemetry();
     handleMAVLinkTelemetry();
+    handleIbusTelemetry();
 }
 
 #endif
